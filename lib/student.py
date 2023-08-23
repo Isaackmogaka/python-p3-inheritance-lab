4249495f77b0d19a1d790cb7d675bac1ca318e34
isaacmogaka <isaacmogaka2016@gmail.com>
2023-08-23 12:19:36 +0300

    Completed Assignment

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-
+# student.py
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize self.knowledge as an empty list
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)  # Add new_knowledge to the knowledge list
+
+
+ 
